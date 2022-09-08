04d41a7d747828d1aa62137dbe339e41ccffa3ac
Yuriy Kazakevich <yury.kazakevic@gmail.com>
2022-09-08 23:17:40 +0300

    Fix client app name

--- src/Backebd/Borigran.OneData.WebClient/Schema.sql
+++ src/Backebd/Borigran.OneData.WebClient/Schema.sql
@@ -1,7 +1,18 @@
 
+    if exists (select * from dbo.sysobjects where id = object_id(N'TUsers') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table TUsers
+
     if exists (select * from dbo.sysobjects where id = object_id(N'TConstructorItems') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table TConstructorItems
 
     if exists (select * from dbo.sysobjects where id = object_id(N'TImages') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table TImages
+
+    create table TUsers (
+        UserID INT IDENTITY NOT NULL,
+       PhoneNumber NVARCHAR(255) null,
+       RefreshToken NVARCHAR(255) null,
+       RefreshTokenExpired DATETIME2 null,
+       primary key (UserID),
+      unique (PhoneNumber)
+    )
 
     create table TConstructorItems (
         ConstructorItemID INT IDENTITY NOT NULL,
